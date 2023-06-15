c9a2a4725cca0bb4c36d87e36bac90f8a85f23ab
Eduardo Rodriguez <eduardo_sdacr@hotmail.com>
2023-06-15 15:55:40 -0500

    feat: complete logout endpoint

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -30,7 +30,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
 
     if (!user) {
-      throw new UnauthorizedException('User not found');
+      throw new UnauthorizedException('Invalid authentication');
     }
 
     return user;
