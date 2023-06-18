a869fefa4e14f4b0c2a4970961f6b7cce597af98
Eduardo Rodriguez <eduardo_sdacr@hotmail.com>
2023-06-18 00:50:45 -0500

    feat: complete delete category endpoint

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
 export class CategoryController {
@@ -8,5 +20,18 @@ export class CategoryController {
   @Get()
   getCategories() {
     return this.categoryService.list();
+  }
+
+  @UseGuards(JwtGuard)
+  @Post()
+  createCategory(@Body() input: CreateCategoryDto) {
+    return this.categoryService.create(input);
+  }
+
+  @UseGuards(JwtGuard)
+  @Delete(':categoryId')
+  @HttpCode(204)
+  async deleteCategory(@Param('categoryId', ParseIntPipe) categoryId: number) {
+    await this.categoryService.delete(categoryId);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CategoryDto } from './dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -8,5 +9,31 @@ export class CategoryService {
 
   async list(): Promise<CategoryDto[]> {
     return this.prisma.category.findMany();
+  }
+
+  async create(input: CreateCategoryDto): Promise<CategoryDto> {
+    return this.prisma.category.create({
+      data: {
+        ...input,
+      },
+    });
+  }
+
+  async delete(categoryId: number): Promise<void> {
+    const category = await this.prisma.category.findUnique({
+      where: {
+        id: categoryId,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    await this.prisma.category.delete({
+      where: {
+        id: categoryId,
+      },
+    });
   }
 }
